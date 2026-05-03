be5d81913a9a6d1e41e77619f2236847bbc8608a
jhon kily valverde <jhonkily007@gmail.com>
2026-05-03 13:35:42 -0700

    pagina para home con sus adicionales

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  constructor(private router: Router) {}
+
+  // 🔥 cerrar sesión
+  logout() {
+    localStorage.removeItem('user');
+    this.router.navigate(['/']);
+  }
+
+}
