c062a272120c4066728ec44fdf01f6a938c322ed
jhon kily valverde <jhonkily007@gmail.com>
2026-05-03 21:06:01 -0700

    angular json

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
