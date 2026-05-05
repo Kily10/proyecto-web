fe0c8d3289f8886d1cc48de2b969d2a5409575f7
jhon kily valverde <jhonkily007@gmail.com>
2026-05-04 17:09:35 -0700

    pag de planes sub

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -14,34 +14,82 @@ export class LoginComponent {
 
   email: string = '';
   password: string = '';
+  modoRegistro: boolean = false; // 🔥 cambiar entre login / registro
 
   constructor(private router: Router) {}
 
+  ngOnInit() {
+    const user = localStorage.getItem('user');
+    if (user) {
+      this.router.navigate(['/home']);
+    }
+  }
+
+  // 🔥 LOGIN
   login() {
 
-    // 🔴 VALIDACIÓN BÁSICA
     if (!this.email || !this.password) {
       alert('Completa todos los campos');
       return;
     }
 
-    // 🔥 USUARIO SIMULADO
-    if (this.email === 'admin@test.com' && this.password === '1234') {
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
 
-      const user = {
-          id: 1,
-          email: this.email,
-          loginTime: new Date().toLocaleString()
-      };
+    const existe = users.find(
+      (u: any) => u.email === this.email && u.password === this.password
+    );
 
-      localStorage.setItem('user', JSON.stringify(user));
-
-      this.router.navigate(['/home']);
-
-    } else {
-      alert('Credenciales incorrectas');
+    if (!existe) {
+      alert('Cuenta no encontrada ❌');
+      return;
     }
 
+    localStorage.setItem('user', JSON.stringify(existe));
+    this.router.navigate(['/home']);
+  }
+
+  // 🔥 REGISTRO
+  register() {
+
+    if (!this.email || !this.password) {
+      alert('Completa los campos');
+      return;
+    }
+
+    if (!this.email.includes('@gmail.com')) {
+      alert('Debe ser Gmail');
+      return;
+    }
+
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
+
+    const existe = users.find((u: any) => u.email === this.email);
+
+    if (existe) {
+      alert('Esta cuenta ya existe ⚠️');
+      return;
+    }
+
+    const newUser = {
+      id: Date.now(),
+      email: this.email,
+      password: this.password,
+      plan: 'Gratis',
+      role: this.email === 'admin@gmail.com' ? 'admin' : 'user',
+      loginTime: new Date().toLocaleString()
+    };
+
+    users.push(newUser);
+    localStorage.setItem('users', JSON.stringify(users));
+
+    alert('Cuenta creada ✅');
+
+    this.modoRegistro = false;
+  }
+
+  // 🔄 cambiar modo
+  toggleModo() {
+    this.modoRegistro = !this.modoRegistro;
   }
 
 }
