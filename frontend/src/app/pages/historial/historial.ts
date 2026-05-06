5685b9a5533ef253da33ac6ab9a094a92e8d41b1
jhon kily valverde <jhonkily007@gmail.com>
2026-05-05 17:31:25 -0700

    hitorial html

--- frontend/src/app/pages/historial/historial.ts
+++ frontend/src/app/pages/historial/historial.ts
@@ -14,16 +14,17 @@ export class HistorialComponent {
   historial: any[] = [];
   user: any;
 
+  private API = 'http://127.0.0.1:8000/api';
+
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
+    ngOnInit() {
+      this.user = JSON.parse(localStorage.getItem('user') || '{}');
 
-    this.user = JSON.parse(localStorage.getItem('user') || '{}');
-
-    this.http.get(`http://127.0.0.1:8000/api/historial/${this.user.id}`)
-      .subscribe((data: any) => {
-        this.historial = data;
-      });
+      this.http.get(`${this.API}/historial/${this.user.id}`)
+       .subscribe((data: any) => {
+         this.historial = data;
+       });
   }
 
 }
